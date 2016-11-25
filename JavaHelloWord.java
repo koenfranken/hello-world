cde6ab51a400f13ca516bcdb7293d7e2047877cc
Koen Franken <koen.franken@yahoo.com>
2016-11-25 14:56:33 +0100

    Dockerfile

---
+++ JavaHelloWord.java
@@ -0,0 +1,9 @@
+public class JavaHelloWorld
+ {
+  public static void main (String [ ] args)
+  {
+System.out.println("Java hello world");
+System.out.println("He really is");
+System.out.println("No seriously, he's really cool");
+}
+}
