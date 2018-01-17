aa6996668d6125ec607a729d3e512f109c65bb94
koenfranken <koen.franken@yahoo.com>
2018-01-17 16:23:33 +0100

    Update JavaHelloWorld.java

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -2,7 +2,7 @@ public class JavaHelloWorld
  {
   public static void main (String [ ] args)
   {
-    System.out.println("Java Hello World");
+    System.out.println("Java Earth");
     System.out.println("He really is");
     System.out.println("No seriously, he's really cool");
   }
