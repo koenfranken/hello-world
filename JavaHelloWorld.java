9f4c169962dd697a22ece365d6db1b5435e65ddb
koenfranken <koen.franken@yahoo.com>
2018-01-17 16:04:38 +0100

    Update JavaHelloWorld.java
    Indentation

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -2,8 +2,8 @@ public class JavaHelloWorld
  {
   public static void main (String [ ] args)
   {
-System.out.println("Java Hello World");
-System.out.println("He really is");
-System.out.println("No seriously, he's really cool");
-}
+    System.out.println("Java Hello World");
+    System.out.println("He really is");
+    System.out.println("No seriously, he's really cool");
+  }
 }
